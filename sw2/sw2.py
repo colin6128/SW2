2175b0f182d2b12d54fe62e2d899da1a53be75c2
colin gittings <colin@6128.co.uk>
2017-09-24 22:38:51 +0100

    Added Pause screen and 'End game' screen, quit key function in-game, game icon

--- sw2/sw2.py
+++ sw2/sw2.py
@@ -24,12 +24,18 @@ block_color = (53, 115, 255)
 
 car_width = 53
 
+pause = False
+
 # create window
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('A Bit Racey')
 clock = pygame.time.Clock()
 
 carImg = pygame.image.load('images/racecar.png')  # car dims 53 x 80 pixels
+gameIcon = pygame.image.load('images/racecar_icon.png')
+# icon dims 32x32 pixels
+
+pygame.display.set_icon(gameIcon)
 
 
 def things_dodged(count):
@@ -65,7 +71,26 @@ def message_display(text):
 
 
 def crash():
-    message_display('You Crashed')
+    crash = True
+
+    while crash:
+        for event in pygame.event.get():
+            # print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        # gameDisplay.fill(white)
+        largeText = pygame.font.Font('freesansbold.ttf', 115)
+        TextSurf, TextRect = text_objects("You Crashed", largeText)
+        TextRect.center = ((display_width/2), (display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+
+        button("Start Over", 150, 450, 150, 50, green, bright_green, game_loop)
+        button("End Game", 550, 450, 150, 50, red, bright_red, quitgame)
+
+        pygame.display.update()
+        clock.tick(15)
 
 
 def button(msg, x, y, w, h, ic, ac, action=None):
@@ -76,7 +101,7 @@ def button(msg, x, y, w, h, ic, ac, action=None):
     if x + w > mouse[0] > x and y + h > mouse[1] > y:
         pygame.draw.rect(gameDisplay, ac, (x, y, w, h))
         if click[0] == 1 and action is not None:
-            print(msg)
+            print(msg + ' button')
             action()
 
     else:
@@ -89,8 +114,36 @@ def button(msg, x, y, w, h, ic, ac, action=None):
 
 
 def quitgame():
+    print("quit")
     pygame.quit()
     quit()
+
+
+def unpause():
+    global pause
+    pause = False
+    pygame.mixer.music.unpause()
+
+
+def paused():
+    while pause:
+        for event in pygame.event.get():
+            # print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        gameDisplay.fill(white)
+        largeText = pygame.font.Font('freesansbold.ttf', 115)
+        TextSurf, TextRect = text_objects("Paused", largeText)
+        TextRect.center = ((display_width/2), (display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+
+        button("Continue", 150, 450, 150, 50, green, bright_green, unpause)
+        button("Quit", 550, 450, 100, 50, red, bright_red, quitgame)
+
+        pygame.display.update()
+        clock.tick(15)
 
 
 def game_intro():
@@ -106,7 +159,7 @@ def game_intro():
 
         gameDisplay.fill(white)
         largeText = pygame.font.Font('freesansbold.ttf', 115)
-        TextSurf, TextRect = text_objects("A Bit Racy", largeText)
+        TextSurf, TextRect = text_objects("A Bit Racey", largeText)
         TextRect.center = ((display_width/2), (display_height/2))
         gameDisplay.blit(TextSurf, TextRect)
 
@@ -118,6 +171,8 @@ def game_intro():
 
 
 def game_loop():
+    global pause
+
     x = (display_width * 0.45)
     y = (display_height * 0.8)
 
@@ -148,6 +203,14 @@ def game_loop():
                     x_change = -5
                 elif event.key == pygame.K_RIGHT:
                     x_change = 5
+
+                if event.key == pygame.K_p:
+                    pause = True
+                    print("pause key")
+                    paused()
+
+                if event.key == pygame.K_q:
+                    quitgame()
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
