457d2d7426fb62bdd98f6245307a697e655c0a07
colin gittings <colin@6128.co.uk>
2017-10-02 07:58:58 +0100

    updated bonus/shump to have collisions and bullets

--- sw2/bonus/shmup.py
+++ sw2/bonus/shmup.py
@@ -62,6 +62,11 @@ class Player(pygame.sprite.Sprite):
         if self.rect.left < 0:
             self.rect.left = 0
 
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
 
 class Mob(pygame.sprite.Sprite):
     # sprite for the enemies - computer controlled sprites
@@ -73,18 +78,41 @@ class Mob(pygame.sprite.Sprite):
         self.rect.x = random.randrange(0, WIDTH - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
         self.speedy = random.randrange(1, 8)
+        self.speedx = random.randrange(-3, 3)
 
     def update(self):
+        self.rect.x += self.speedx
         self.rect.y += self.speedy
-        if self.rect.top > HEIGHT + 10:
+        if (self.rect.top > HEIGHT + 10
+                or self.rect.left < 25
+                or self.rect.right > WIDTH + 20):
             self.rect.x = random.randrange(0, WIDTH - self.rect.width)
             self.rect.y = random.randrange(-100, -40)
             self.speedy = random.randrange(1, 8)
 
 
+class Bullet(pygame.sprite.Sprite):
+    # sprite for the bullet shot by player
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 20))  # simple starting sprite
+        self.image.fill(yellow)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        # kill if moves off top of the screen
+        if self.rect.bottom < 0:
+            self.kill()
+
+
 # set up sprite group
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
 for i in range(8):
@@ -102,13 +130,30 @@ while running:
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
 
     # Update
     all_sprites.update()
 
-    # Draw / render
+    # check to see if a bullet hit a mob
+    hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+    for hit in hits:
+        m = Mob()
+        all_sprites.add(m)
+        mobs.add(m)
+
+    # check to see if a mob hit the player
+    hits = pygame.sprite.spritecollide(player, mobs,  False)
+    if hits:
+        running = False
+
+# Draw / render
     screen.fill(black)
     all_sprites.draw(screen)
 
-    # after drawing everythng, flip the display
+    # after drawing ever  ythng, flip the display
     pygame.display.flip()
+
+pygame.quit()
